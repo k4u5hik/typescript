64093fb12f9a219a263f1d92bbab99850dfd2dfc
Kaushik Chemburkar <attherate@gmail.com>
2022-05-04 13:59:10 +1000

    exporting module

---
+++ Intro/1-greeter.ts
@@ -0,0 +1,18 @@
+// function greeter(person) {
+//   return "Hello, " + person;
+// }
+// let user = "Jane User";
+// document.body.textContent = greeter(user);
+// console.log(greeter(user));
+
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement;
+const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
+});
---
+++ Intro/2-export.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.b = exports.a = void 0;
+var a = 1;
+exports.a = a;
+var b = 2;
+exports.b = b;

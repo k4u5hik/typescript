a3bb0f15f403b17a2747da68c212c1690549d267
Kaushik Chemburkar <attherate@gmail.com>
2022-05-05 10:59:28 +1000

    refreshing what i learnt - TS basics

--- Intro/src/1-greeter.js
+++ Intro/src/1-greeter.js
@@ -4,12 +4,18 @@
 // let user = "Jane User";
 // document.body.textContent = greeter(user);
 // console.log(greeter(user));
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
-    return num1 + num2;
-}
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+var hello = "world";
+hello = "foo";
+var getFullName = function (firstName, lastName) {
+    return firstName + " " + lastName;
+};
+console.log(getFullName("John", "Goe"));
